fcf1ca6f1ece34aa37b1d351d7ee81b2f47b7e35
Daniel Gozálvez Sánchez <dgs65@alu.ua.es>
2024-06-24 08:45:24 +0200

    Third Publish V0.0.0.4

--- AutoUpdaterNetTest/Form1.Designer.cs
+++ AutoUpdaterNetTest/Form1.Designer.cs
@@ -30,11 +30,13 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(311, 115);
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(244, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(164, 20);
+            this.label1.Size = new System.Drawing.Size(322, 45);
             this.label1.TabIndex = 0;
-            this.label1.Text = "AutoUpdater V0.0.0.2";
+            this.label1.Text = "AutoUpdater V0.0.0.3";
             // 
             // textBox1
             // 
@@ -47,9 +49,11 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(70)))), ((int)(((byte)(70)))), ((int)(((byte)(80)))));
             this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
